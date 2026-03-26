f705f103797112d3c99ad4a7c4e241b2e2c4e784
Talha Sheikh <atiqtalha3@gmail.com>
2026-03-26 19:51:11 +0500

    work on talent

--- wp-content/themes/tta/functions.php
+++ wp-content/themes/tta/functions.php
@@ -146,6 +146,7 @@ function tta_scripts() {
 	wp_enqueue_style('tta-home-style', get_template_directory_uri().'/css/new-homepage.css', array(), time());
 	wp_enqueue_style('tta-contact-style', get_template_directory_uri().'/css/new-contact-page.css', array(), time());
 	wp_enqueue_style('tta-faq-style', get_template_directory_uri().'/css/new-faqs.css', array(), time());
+	wp_enqueue_style('tta-talent-hero-style', get_template_directory_uri().'/css/talent-hero.css', array(), time());
 	// wp_enqueue_style('tta-slick-theme-style', get_template_directory_uri().'/css/new-homepage.css', array(), time());
 
 	// wp_enqueue_script( 'jquery');
